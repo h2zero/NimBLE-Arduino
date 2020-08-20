7ab51ef3a53ac8d1e333982b5a3a81e99702c2fd
Roland Szabo <r.szabo@mohanet.com>
2020-08-20 09:42:51 -0600

    Fix incorrect buffer index used when processing longer writes

--- src/NimBLECharacteristic.cpp
+++ src/NimBLECharacteristic.cpp
@@ -240,7 +240,7 @@ int NimBLECharacteristic::handleGapEvent(uint16_t conn_handle, uint16_t attr_han
                     if((len + next->om_len) > BLE_ATT_ATTR_MAX_LEN) {
                         return BLE_ATT_ERR_INVALID_ATTR_VALUE_LEN;
                     }
-                    memcpy(&buf[len-1], next->om_data, next->om_len);
+                    memcpy(&buf[len], next->om_data, next->om_len);
                     len += next->om_len;
                     next = SLIST_NEXT(next, om_next);
                 }
