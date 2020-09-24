72f728b54653d7c9a1e343d8634afafbeaf082f6
h2zero <powellperalta@gmail.com>
2020-09-23 19:37:02 -0600

    Remove NimBLEMeshModel::getHealth
    Code change made getHealth unnecessary.

--- src/NimBLEMeshElement.cpp
+++ src/NimBLEMeshElement.cpp
@@ -58,7 +58,7 @@ NimBLEMeshModel* NimBLEMeshElement::createModel(uint16_t type, NimBLEMeshModelCa
         case BT_MESH_MODEL_ID_HEALTH_SRV:
             m_pHealthModel = new NimBLEHealthSrvModel(pCallbacks);
             pModel = m_pHealthModel;
-            m_modelsVec.push_back(bt_mesh_model{{BT_MESH_MODEL_ID_HEALTH_SRV},0,0,0,&pModel->m_opPub,{0},{0},bt_mesh_health_srv_op,pModel->getHealth_t()});
+            m_modelsVec.push_back(bt_mesh_model{{BT_MESH_MODEL_ID_HEALTH_SRV},0,0,0,&pModel->m_opPub,{0},{0},bt_mesh_health_srv_op,&m_pHealthModel->m_healthSrv});
             return pModel;
 
         default:
