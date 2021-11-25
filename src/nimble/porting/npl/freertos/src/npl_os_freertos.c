4a6111d9639053caddc398f67b3dd455b26eb3aa
h2zero <32826625+h2zero@users.noreply.github.com>
2021-11-24 19:27:18 -0700

    Workaround for xTimerIsTimerActive bug. (#311)
    This adds a workaround for a bug in FreeRTOS xTimerIsTimerActive function that would return true when the timer has expired.
    
    This bug was introduced in FreeRTOS V10.2.0, and fixed in V10.4.4. 
    
    See: https://github.com/FreeRTOS/FreeRTOS-Kernel/pull/305

--- src/nimble/porting/npl/freertos/src/npl_os_freertos.c
+++ src/nimble/porting/npl/freertos/src/npl_os_freertos.c
@@ -517,7 +517,11 @@ npl_freertos_callout_is_active(struct ble_npl_callout *co)
 #if CONFIG_BT_NIMBLE_USE_ESP_TIMER
     return esp_timer_is_active(co->handle);
 #else
-    return xTimerIsTimerActive(co->handle) == pdTRUE;
+    /* Workaround for bug in xTimerIsTimerActive with FreeRTOS V10.2.0, fixed in V10.4.4
+     * See: https://github.com/FreeRTOS/FreeRTOS-Kernel/pull/305
+     * Sometimes xTimerIsTimerActive returns pdTRUE even though the timer has expired, so we double check.
+     */
+    return xTimerIsTimerActive(co->handle) == pdTRUE && xTimerGetExpiryTime(co->handle) > xTaskGetTickCountFromISR();
 #endif
 }
 
