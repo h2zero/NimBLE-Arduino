8e2c4e40c7496fe45d2fa55ec79322038012d2a7
h2zero <powellperalta@gmail.com>
2022-06-26 17:06:00 -0600

    Bugfix/onRead callback not called for non-long read commands.

--- src/NimBLECharacteristic.cpp
+++ src/NimBLECharacteristic.cpp
@@ -273,11 +273,13 @@ int NimBLECharacteristic::handleGapEvent(uint16_t conn_handle, uint16_t attr_han
     if(ble_uuid_cmp(uuid, &pCharacteristic->getUUID().getNative()->u) == 0){
         switch(ctxt->op) {
             case BLE_GATT_ACCESS_OP_READ_CHR: {
-                // If the packet header is only 8 bytes this is a follow up of a long read
-                // so we don't want to call the onRead() callback again.
-                if(ctxt->om->om_pkthdr_len > 8) {
-                    rc = ble_gap_conn_find(conn_handle, &desc);
-                    assert(rc == 0);
+                rc = ble_gap_conn_find(conn_handle, &desc);
+                assert(rc == 0);
+
+                 // If the packet header is only 8 bytes this is a follow up of a long read
+                 // so we don't want to call the onRead() callback again.
+                if(ctxt->om->om_pkthdr_len > 8 ||
+                   pCharacteristic->m_value.size() <= (ble_att_mtu(desc.conn_handle) - 3)) {
                     pCharacteristic->m_pCallbacks->onRead(pCharacteristic);
                     pCharacteristic->m_pCallbacks->onRead(pCharacteristic, &desc);
                 }
