0e8a2c4e25d3a9ebce4bf3e4c3ba5755454cec51
h2zero <ryan@nable-embedded.io>
2024-12-24 11:21:41 -0700

    Sync with esp-nimble-cpp.

--- src/NimBLEUtils.cpp
+++ src/NimBLEUtils.cpp
@@ -37,6 +37,9 @@
 # include <climits>
 
 # if defined INC_FREERTOS_H
+#  ifndef CONFIG_NIMBLE_CPP_FREERTOS_TASK_BLOCK_BIT
+#   define CONFIG_NIMBLE_CPP_FREERTOS_TASK_BLOCK_BIT 31
+#  endif
 constexpr uint32_t TASK_BLOCK_BIT = (1 << CONFIG_NIMBLE_CPP_FREERTOS_TASK_BLOCK_BIT);
 # endif
 
